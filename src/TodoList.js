d9020d4e5052db6dafc7cba2b09b6fcae137e740
wangwenhui <981837467@qq.com>
2021-05-25 19:10:21 +0800

    代码优化，相关JSX语法

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,11 @@
 import React,{Component,Fragment} from "react";
+import './style.css'
 class TodoList extends Component{
   constructor(props){
     super(props);
+    //函数用了bind之后每次执行都会新建一个 开销较大，性能优化.传了参数的bind无法优化
+    this.handleInputChange=this.handleInputChange.bind(this);
+    this.handleKeyUp=this.handleKeyUp.bind(this);
     this.state={
       inputValue:'初始值',
       list:[]
@@ -28,18 +32,30 @@ class TodoList extends Component{
       list
     })
   }
+  getListItems(){//优化html结构
+    return this.state.list.map((item,idx)=>{
+      return (<li
+                 key={idx} 
+                 onClick={this.handleItemClick.bind(this,idx)}
+                 dangerouslySetInnerHTML={{__html:item}}
+                 >
+                 </li>)
+    })
+  }
   render(){
     return(
       <Fragment>
-        <input  
+        {/* JSX的注释 */}
+        <label htmlFor='myinput'>请输入内容:</label>
+        <input
+        id='myinput'
+          className='input'
           value={this.state.inputValue} 
-          onChange={this.handleInputChange.bind(this)}
-          onKeyUp={this.handleKeyUp.bind(this)}
+          onChange={this.handleInputChange}
+          onKeyUp={this.handleKeyUp}
           />
         <ul>
-          {this.state.list.map((item,idx)=>{
-            return <li key={idx} onClick={this.handleItemClick.bind(this,idx)}>{item}</li>
-          })}
+          {this.getListItems()}
         </ul>
       </Fragment> 
     )
