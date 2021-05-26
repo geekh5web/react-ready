aade261a8c738dd1becee5fa40e9784cf56d278e
wangwenhui <981837467@qq.com>
2021-05-26 15:48:32 +0800

    1-2父子组件传值

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,10 +5,13 @@ constructor(props){
   this.handleItemClick=this.handleItemClick.bind(this)
 }
 handleItemClick(){
-  console.log('handleItemClick')
+  const {deleteFunction,index}=this.props;
+  deleteFunction(index);
+  // this.props.deleteFunction(this.props.index)
 }
   render(){
     //子组件通过this.props的属性接收从父组件传递过来的值
+    //子组件想要和父组件通信，他要调用父组件传递过来的方法，方法的作用域需要变更
     const {content}=this.props;//es6写法，等价于const content=this.props.content
     return(
       <li onClick={this.handleItemClick}>{content}</li>
--- src/TodoList.js
+++ src/TodoList.js
@@ -7,6 +7,7 @@ class TodoList extends Component{
     super(props);
     this.handleChange=this.handleChange.bind(this);
     this.handleKeyUp=this.handleKeyUp.bind(this);
+    this.handleItemClick=this.handleItemClick.bind(this);//固定this指向，需要传递给子组件
     this.state={
       inputValue:'初始值',
       list:['1','2','3','4','5','6','7']
@@ -27,7 +28,7 @@ class TodoList extends Component{
       })
     }
   }
-  handleClick(idx){
+  handleItemClick(idx){
     const list=[...this.state.list];
     list.splice(idx,1);
     this.setState({
@@ -37,7 +38,7 @@ class TodoList extends Component{
   }
   getListItem(){
     return this.state.list.map((item,idx)=>{
-      return <TodoItem content={item} key={idx}/>
+      return <TodoItem content={item} key={idx} index={idx} deleteFunction={this.handleItemClick}/>
     })
   }
   render(){
